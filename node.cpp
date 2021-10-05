5a2b358799cc320197c43cf57666e24781b4ea1e
MarkSchwe <91101294+MarkSchwe@users.noreply.github.com>
2021-10-04 21:40:59 -0400

    Update node.cpp

--- node.cpp
+++ node.cpp
@@ -2,8 +2,10 @@
 #include <iostream>
 #include "node.h"
 using namespace std;
+//constructor for the node
 Node::Node(){}
 
+//function that prints the firstname, lastname, and id number of the node.
 void Node::printNode(){
 cout << "Student First Name: " << firstName << endl;
 cout << "Student Last Name: " << lastName << endl;
