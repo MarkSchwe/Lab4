86fc85f71dce32020efccb5617ff422d70c6a5cd
MarkSchwe <91101294+MarkSchwe@users.noreply.github.com>
2021-10-04 21:33:40 -0400

    Create LinkedList.h

---
+++ LinkedList.h
@@ -0,0 +1,21 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+#include "node.h"
+#include <iostream>
+using namespace std;
+
+class LinkedList{
+private:
+Node *listHead;
+Node *createNode();
+Node *searchLocation(int ID);
+
+public:
+LinkedList();
+~LinkedList();
+void insertNode();
+void deleteNode(int ID);
+void printList();
+void searchNode();
+};
+#endif
