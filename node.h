6eaf5e6a70a08a73ac489462539aed2fd414a486
MarkSchwe <91101294+MarkSchwe@users.noreply.github.com>
2021-10-04 21:32:16 -0400

    Create node.h

---
+++ node.h
@@ -0,0 +1,16 @@
+#ifndef NODE_H
+#define NODE_H
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+char lastName[20];
+char firstName[20];
+int idNumber;
+Node *Next;
+void printNode();
+Node();
+};
+
+
+#endif
