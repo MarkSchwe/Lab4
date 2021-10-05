7a0c90d87c8591c54ca6c7a60129b19d71d7cf9b
MarkSchwe <91101294+MarkSchwe@users.noreply.github.com>
2021-10-04 21:32:50 -0400

    Create node.cpp

---
+++ node.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include "node.h"
+using namespace std;
+Node::Node(){}
+
+void Node::printNode(){
+cout << "Student First Name: " << firstName << endl;
+cout << "Student Last Name: " << lastName << endl;
+cout << "Student ID: " << idNumber << endl;
+
+
+}
