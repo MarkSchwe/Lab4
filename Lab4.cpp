8232f42826c2fe9c723bd283a963a2b43aa404a7
MarkSchwe <91101294+MarkSchwe@users.noreply.github.com>
2021-10-04 21:44:01 -0400

    Update Lab4.cpp

--- Lab4.cpp
+++ Lab4.cpp
@@ -8,27 +8,34 @@ int main(){
     //sentinel value that keeps the loop going until it equals 5
     int sentinel = 1;
     int ID;
+    //while loop that continues the loop
     while(sentinel != 5){
+        //loop that continues the loop as long as the input is between two specific numbers.
         while (sentinel > 0 && sentinel < 5){
         cout << "Menu options:" << endl << "1: Insert a node" << endl << "2: Delete a node" << endl << "3: Print List" << endl << "4: Search a Node" << endl << "5: Quit the program" << endl;
         cin >> sentinel;
         if (sentinel < 0 || sentinel > 5){
             cout << "Invalid input." << endl;
         }
+            //inserts a node
         if (sentinel == 1){
             newList.insertNode();
         }
+            //asks for an ID for a node then deletes it.
         if (sentinel == 2){
             cout << "Input ID to delete: ";
             cin >> ID;
             newList.deleteNode(ID);
         }
+            //prints newList
         if (sentinel == 3){
             newList.printList();
         }
+            //searches the nodes in newList
         if (sentinel == 4){
             newList.searchNode();
         }
+            //ends the program
         if(sentinel == 5){
             return 0;
         }
